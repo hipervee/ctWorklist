e91bfec86788cdd9f8db42f9e87097244736dc56
Pervaze Ahanger <hipervee@gmail.com>
2018-07-07 17:32:43 +0530

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routing';
 import { LoginComponent } from './login/login.component';
-
-
+import { GridComponent } from './grid/grid.component';
+import { NavComponent } from './nav/nav.component';
+import { Angular2PromiseButtonModule } from 'angular2-promise-buttons/dist';
+import { PriorityService } from './services/priority.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+const asynButtonConfig = {
+  // your custom config goes here
+  spinnerTpl: '<span class="btn-spinner"></span>',
+  // disable buttons when promise is pending
+  disableBtn: true,
+  // the class used to indicate a pending promise
+  btnLoadingClass: 'is-loading',
+  // only disable and show is-loading class for clicked button, 
+  // even when they share the same promise
+  handleCurrentBtnOnly: false,
+};
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    GridComponent,
+    NavComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    Angular2PromiseButtonModule.forRoot(asynButtonConfig),
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
   ],
-  providers: [],
+  providers: [PriorityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
